30ca2936701a1524214b973a442a0630f686671c
Jason Smith <jasondsmith2501@gmail.com>
2025-01-07 16:10:10 -0800

    Finished solution

--- solution.js
+++ solution.js
@@ -18,3 +18,14 @@ document.getElementById("task5").href = "https://www.springboard.com/";
 
 // Task 6
 document.getElementById("task6").value = "DOM Master";
+
+// Task 7
+document.getElementById("task7").classList.add("new-class");
+
+// Task 8
+const newButton = document.createElement("button");
+newButton.textContent = "Click";
+document.getElementById("task8").appendChild(newButton);
+
+// Task 9
+document.getElementById("task9").remove();
